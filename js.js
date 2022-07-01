c3e2991226e55d1c6fa7c8c24699085e95acbd42
bineshklb <102851480+bineshklb@users.noreply.github.com>
2022-07-01 22:34:42 +0530

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function validate()
 {
 var username=document.getElementById("username").Value;
 var password=document.getElementById("password").Value;
-if(username=="binesh"&& password=="123")
+if(username=="binesh" && password=="123")
 {
     alert("login successfully");
     return false;    
