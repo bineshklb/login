605454721c6a9ce91da15e4a663b42186d113410
bineshklb <102851480+bineshklb@users.noreply.github.com>
2022-07-01 22:47:49 +0530

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function validate()
 {
 var username=document.getElementById("username").Value;
 var password=document.getElementById("password").Value;
-if(username=="binesh"&& password=="123")
+if(username=="binesh"&& password=="binu")
 {
     alert("login successfully");
     return false;    
@@ -12,9 +12,4 @@ else
     alert("login failed");
 }
 }
-// function makeaTea();
-// console.log("step1=boiled water");
-// console.log("step2=add milk");
-// console.log("step3=put tea powder");
-// console.log("step4=add sugar");
-// makeaTea();
+
